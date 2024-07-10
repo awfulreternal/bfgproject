1ad8ccfb19aaa4b069e7c58714739c9e11f74660
awfulreternal <matveykudelin7@gmail.com>
2024-07-10 20:01:30 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+API_TOKEN=:'7418309682:AAGWQBdmpEj0OWcifVbIhdvEWjLSDtAL3Ws'
+
+admin = [@act_mk]
+start_money = 10000
+
+bot_name = 'BFG'
+chat = 't.me/copybfg_chat'
+chanell = 't.me/bfgproject_dev'
+admin_username = '@act_mk'
+bot_username = '@copybfg_bot'
+
+chat_log = 0
+cleaning = 60
